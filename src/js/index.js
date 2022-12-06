eef29e45c71132dcae6a7154ba6ce8fffc5e0b1c
sofoklisfountas <sofoklisfountas@gmail.com>
2022-12-06 03:32:47 +0200

    Added footer component. Finalized some components. Needs refactoring

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,4 @@ import CardContainer from './wc/card-container-wc';
 import Card from './wc/card-wc';
 import HeaderWc from './wc/header-wc';
 import ProjectCardWc from './wc/home-page/project-card-wc';
+import FooterWc from './wc/footer-wc';
